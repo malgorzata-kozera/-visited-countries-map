7456f2de60fca32aa14aa7d3dc52e7edbdc67774
Tsunade <malgorzata.kozera5@gmail.com>
2019-04-10 13:09:48 +0200

    some

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = '^d4o(hos+xfhf34e!7v8er5mz0)bbzm#5tf=y0a#z%xsjb1(jm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'database_map',
-        'USER': 'postgres_user',
-        'PASSWORD':'test123',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -132,3 +132,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
