942b312c17b93c1bc0a385c8e06b72855c418880
Tsunade <malgorzata.kozera5@gmail.com>
2019-04-10 14:55:15 +0200

    heroku added to allowed hosts

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://www.heroku.com']
+ALLOWED_HOSTS = ['https://www.heroku.com',".herokuapp.com"]
 
 
 # Application definition
