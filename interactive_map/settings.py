d97406b2004df42bc9f86328fed23319e11695d0
Tsunade <malgorzata.kozera5@gmail.com>
2019-04-10 15:03:28 +0200

    colectstatic

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://www.heroku.com',".herokuapp.com"]
 
