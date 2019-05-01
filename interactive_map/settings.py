fc476ccba70dd13b94895ffd81f2b4c3487cc190
Tsunade <malgorzata.kozera5@gmail.com>
2019-05-01 14:49:36 +0200

    debug true

--- interactive_map/settings.py
+++ interactive_map/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://www.heroku.com','.herokuapp.com','127.0.0.1']
 
